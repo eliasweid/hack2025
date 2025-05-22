f5718e0beb210b1d601ffd3e0730ac4113a310c7
eliasweid <elias.weid@hotmail.com>
2025-05-22 19:15:38 +0200

    IMG

--- src/app/page.js
+++ src/app/page.js
@@ -10,8 +10,8 @@ export default function Home() {
     <div className={styles.page}>
       
       <main className={styles.main}>
-       <ProfileCard name="Elias Weid" progress={75} avatar = "/public/elias.png" />
-       <ProfileCard name="Erik Stolpe" progress={43} avatar = "/public/erik.png"/>
+       <ProfileCard name="Elias Weid" progress={75} avatar = "/elias.png" />
+       <ProfileCard name="Erik Stolpe" progress={43} avatar = "/erik.png"/>
        <ProfileCard name="Ella Bernsfelt" progress={90} avatar="/ella.png"/>
        <ProfileCard name="Elliot Storm" progress={10} avatar = "/profile.png"/>
        <ProfileCard name="Love Broberg" progress={75} />
